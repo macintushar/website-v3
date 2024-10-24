afebb6b29d4df5af54f0e12179fbd18eddd6dd9c
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-10-25 01:33:50 +0530

    added projects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,46 @@
 export const socialLinks = {
-    github: 'https://github.com/macintushar',
-    linkedin: '',
-}
+	github: 'https://github.com/macintushar',
+	linkedin: ''
+};
+
+export type ProjectLink = {
+	name: string;
+	url?: string;
+	github?: string;
+	description?: string;
+	ctaText?: string;
+	date?: string;
+	heroImage?: string;
+	tags?: string[];
+	isFeatured?: boolean;
+};
+
+export const projects: ProjectLink[] = [
+	{
+		name: 'Draw',
+		url: 'https://draw.macintushar.xyz',
+		github: 'https://github.com/macintushar/draw',
+		description:
+			'The digital drawing tool that enables you to create, edit, and share your drawings across all your devices.',
+		date: '2024',
+		heroImage: 'https://draw.macintushar.xyz/draw-preview-img.png',
+		tags: ['React', 'TypeScript', 'Excalidraw', 'Supabase'],
+		ctaText: 'Try Draw',
+		isFeatured: true
+	},
+	{
+		name: 'SPKR - Voice Keyboard',
+		github: 'https://github.com/macintushar/SPKR-Voice-Keyboard',
+		description: 'SPKR is a keyboard app that turns voice input into translated text.',
+		heroImage: 'https://utfs.io/f/94b46082-327a-43b3-9c93-327a45638769-1ip38.png',
+		date: '2023'
+	},
+	{
+		name: 'Sneaker Street',
+		github: 'https://github.com/macintushar/SneakerStore',
+		heroImage: 'https://utfs.io/f/94b46082-327a-43b3-9c93-327a45638769-1ip38.png',
+		description:
+			'Sneaker Street is an e-commerce platform intended on helping users to easily purchase sneakers.',
+		date: '2023'
+	}
+];
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { Badge } from '$lib/components/ui/badge';
+	import { Button } from '$lib/components/ui/button';
+	import {
+		Card,
+		CardHeader,
+		CardContent,
+		CardDescription,
+		CardTitle,
+		CardFooter
+	} from '$lib/components/ui/card';
+	import { projects, socialLinks } from '$lib/data';
+</script>
+
+{#snippet link(/** @type {string | undefined} */ url, /** @type {string} */ text)}
+	<a href={url} target="_blank">
+		<Button class="font-bold" variant="outline" size="sm">{text}</Button>
+	</a>
+{/snippet}
+
+<div class="flex flex-col space-y-5">
+	<h1>
+		Below is a list of some of the fun projects I've worked on. You can check out my <a
+			class="font-semibold text-orange-500 underline"
+			href={socialLinks.github}>GitHub</a
+		> for more.
+	</h1>
+	{#each projects as project}
+		<Card>
+			<div class="w-full">
+				<CardHeader class="flex flex-col space-y-5">
+					{#if project.isFeatured}
+						{#if project.heroImage && project.heroImage !== ''}
+							<img src={project.heroImage} alt={project.name} />
+						{/if}
+						{#if project.tags}
+							<div class="flex flex-wrap gap-2">
+								{#each project.tags as tag}
+									<Badge variant="outline">{tag}</Badge>
+								{/each}
+							</div>
+						{/if}
+					{/if}
+				</CardHeader>
+				<CardContent>
+					<CardTitle class="flex gap-1">
+						<h1>{project.name}</h1>
+						<h2 class="text-sm font-medium text-orange-400">
+							/ {project.date || '1970'}
+						</h2>
+					</CardTitle>
+					<CardDescription>
+						<p>{project.description}</p>
+					</CardDescription>
+				</CardContent>
+				<CardFooter class="flex flex-row-reverse">
+					<div class="flex space-x-4">
+						{@render link(project.github, 'GitHub')}
+						{#if project.url}
+							{@render link(project.url, project.ctaText || 'Demo')}
+						{/if}
+					</div>
+				</CardFooter>
+			</div>
+		</Card>
+	{/each}
+</div>
