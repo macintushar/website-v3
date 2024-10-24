6e53c02fe8c449a85d2f10e079763db3bc13487b
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-10-25 01:33:17 +0530

    updated config url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Tushar Selvakumar'
 export const description = 'Tushar Selvakumar\'s personal homepage'
-export const url = dev ? 'http://localhost:5173/' : 'https://joyofcode.xyz/'
+export const url = dev ? 'http://localhost:5173/' : 'https://macintushar.xyz/'
