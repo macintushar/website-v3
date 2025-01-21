715398e4b784fc2e1c43cd33b65b1a091d0f6d61
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-01-21 17:43:28 +0530

    added year support

--- src/lib/views/footer.svelte
+++ src/lib/views/footer.svelte
@@ -1,10 +1,15 @@
 <script>
 	import { Github, Linkedin } from 'lucide-svelte';
 	import { socialLinks } from '$lib/data';
+	
+	const startYear = 2024;
+	const currentYear = new Date().getFullYear();
+
+	const displayYear = currentYear > startYear ? `${startYear} - ${currentYear}` : startYear;
 </script>
 
 <footer class="flex flex-col justify-center space-y-2 pb-3">
-	<h1 class="text-md w-full text-center font-medium">Tushar Selvakumar © 2024</h1>
+	<h1 class="text-md w-full text-center font-medium">Tushar Selvakumar © {displayYear}</h1>
 	<div class="flex w-full justify-center space-x-3">
 		<a href={socialLinks.github} target="_blank" aria-label="Macintushar GitHub">
 			<Github class="h-5 w-5 dark:text-white" />
