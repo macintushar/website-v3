e141302bcf325392cab5728f39f1d24b4e53ba51
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-08-10 22:48:10 +0530

    refactor: update RESUME_URL to use environment variable and change redirect method

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -4,5 +4,5 @@ import type { RequestHandler } from './$types';
 import { RESUME_URL } from '$lib/constants';
 
 export const GET: RequestHandler = () => {
-	return redirect(307, RESUME_URL);
+	throw redirect(307, RESUME_URL);
 };
