678c7361b434ebd63ef678f11546f863a2e97d0d
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-08-10 14:44:48 +0530

    change to GET

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { RESUME_URL } from '$lib/data';
 
-export const POST: RequestHandler = async ({ request }) => {
+export const GET: RequestHandler = async ({ request }) => {
 	return redirect(307, RESUME_URL);
 };
